ff517ea4599a34b99db7b74eb327a80f7ce81067
JustinBench <d00188916@utahtech.edu>
2026-08-25 13:38:21 -0600

    template-made

---
+++ script.js
@@ -0,0 +1,21 @@
+const bio = document.querySelector("#bio")
+const interests = document.querySelector("#interests")
+const goals = document.querySelector("goals")
+
+function showBio() {
+    bio.classList.remove("hidden");
+    interests.classList.add("hidden");
+    goals.classList.add("hidden");
+}
+
+function showInterests() {
+    bio.classList.add("hidden");
+    interests.classList.remove("hidden");
+    goals.classList.add("hidden");
+}
+
+function showGoals() {
+    bio.classList.add("hidden");
+    interests.classList.add("hidden");
+    goals.classList.remove("hidden");
+}
