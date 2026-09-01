c9c4e03048f68ffdbf61a604b3ffb9c3b8323dd3
JustinBench <d00188916@utahtech.edu>
2026-09-01 12:46:52 -0600

    added-comments-for-notes

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const bio = document.querySelector("#bio")
 const interests = document.querySelector("#interests")
 const goals = document.querySelector("#goals")
 
+// querySelector is the first found element, querySelectorAll is all elements that match the selecto
+//      in an array by order that elements are found
+// It's good to use IDs with querySelector and classes with querySelectorAll
+
 function showBio() {
     bio.classList.remove("hidden");
     interests.classList.add("hidden");
